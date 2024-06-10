39fc9b68582db080a5b38ea2789fa8fc9ff8de1e
Jkama5 <jakamataris@wpi.edu>
2024-06-10 18:29:06 -0400

    added 3d plotting (forgot to put this back in before)

--- calculations/plot_results.py
+++ calculations/plot_results.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import pyvista as pv
+import numpy as np
 
 def plot_magnetic_field_vs_current(currents_mA, B_fields_at_center):
     """Plot the magnetic field at the center point as a function of current."""
@@ -10,3 +12,29 @@ def plot_magnetic_field_vs_current(currents_mA, B_fields_at_center):
     plt.legend()
     plt.grid(True)
     plt.show()
+
+
+def plot_3d_model(vertices, faces, loops, target_points, B_fields_at_targets):
+    """Plot a 3D model of the coil, windings, and magnetic field."""
+    plotter = pv.Plotter()
+    plotter.set_background("white")
+
+    # Create a PolyData object for the coil mesh
+    meshviz_coil = pv.PolyData(vertices, np.hstack((np.repeat(3, len(faces))[:, None], faces)))
+    plotter.add_mesh(meshviz_coil, opacity=0.1, color='blue')
+
+    # Add the coil windings (current loops)
+    for loop in loops:
+        loop_polydata = pv.PolyData(loop)
+        plotter.add_mesh(loop_polydata, color='orange', line_width=2)
+
+    # Add target points
+    plotter.add_points(target_points, color='red', point_size=5)
+
+    # Create arrows to represent the magnetic field at each target point
+    for point, vector in zip(target_points, B_fields_at_targets):
+        if vector is not None and np.linalg.norm(vector) > 0:  # Only create arrows for non-zero vectors
+            arrow = pv.Arrow(start=point, direction=vector, scale=0.05)
+            plotter.add_mesh(arrow, color='magenta')
+
+    plotter.show()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
-from calculations.load_save import save_coil_mesh, load_coil_mesh, save_loops, load_loops
+from calculations.load_save import load_coil_mesh, load_loops
 from calculations.magnetic_field_calculations import create_mesh_conductor, calculate_magnetic_field_at_center
-from calculations.plot_results import plot_magnetic_field_vs_current
+from calculations.plot_results import plot_magnetic_field_vs_current, plot_3d_model
 
 # Load coil mesh and loops
 coilmesh_data = load_coil_mesh()
@@ -26,9 +26,24 @@ z = Z.ravel()
 target_points = np.array([x, y, z]).T
 target_points = target_points[np.linalg.norm(target_points, axis=1) < sidelength / 2] + center
 
+# This is used for the plot_magnetic_field_vs_current method
 # Calculate magnetic field at the new center point for currents from 0mA to 100mA
 currents_mA = np.linspace(0, 100, 101)  # 0mA to 100mA in 1mA steps
 B_fields_at_center = calculate_magnetic_field_at_center(loops, target_points, currents_mA)
 
+
+current_mA = 100  # 1 mA
+current_A = current_mA * 1e-3  # Convert mA to A
+
+
+from bfieldtools.line_conductor import LineConductor
+line_conductor = LineConductor(loops=loops)
+B_fields_at_targets = line_conductor.magnetic_field(target_points) * current_A
+
+# Plot the 3D model
+plot_3d_model(coilmesh_data['vertices'], coilmesh_data['faces'], loops, target_points, B_fields_at_targets)
+
+
 # Plot the results
 plot_magnetic_field_vs_current(currents_mA, B_fields_at_center)
+
